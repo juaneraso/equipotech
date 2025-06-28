c1a1681559105f2d8494aaf40b7780470332da08
Mauricio Barrios <pedidoshmb@gmail.com>
2025-06-28 14:01:53 -0500

    cambios Mauricio

--- ciclofor.js
+++ ciclofor.js
@@ -5,3 +5,10 @@ for (let i = 0; i < 5; i++) {
 console.log("hola grupo de desarrollo");
 console.log("hola grupo de desarrollo allisson");
 console.log("hola grupo de desarrollo Julian");
+
+//Saludando a Todos
+function saludar(grupo) {
+  console.log("Hola " + grupo + "👋 Mauricio B");
+}
+
+saludar("grupo");
